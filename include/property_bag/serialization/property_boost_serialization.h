807bec2f83e62a56cc962486db8f2a35c18af4ab
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-04-04 12:20:29 +0200

    remove comments

--- include/property_bag/serialization/property_boost_serialization.h
+++ include/property_bag/serialization/property_boost_serialization.h
@@ -124,19 +124,4 @@ void serialize(
 } //namespace serialization
 } //namespace boost
 
-/*
-EXPORT_PROPERTY_NAMED_TYPE(bool, bool)
-EXPORT_PROPERTY_NAMED_TYPE(int, int)
-EXPORT_PROPERTY_NAMED_TYPE(float, float)
-EXPORT_PROPERTY_NAMED_TYPE(double, double)
-EXPORT_PROPERTY_NAMED_TYPE(std::string, std__string)
-*/
-/*
-EXPORT_PROPERTY_NAMED_TYPE(std::vector<bool>, std__vector_bool)
-EXPORT_PROPERTY_NAMED_TYPE(std::vector<int>, std__vector_int)
-EXPORT_PROPERTY_NAMED_TYPE(std::vector<float>, std__vector_float)
-EXPORT_PROPERTY_NAMED_TYPE(std::vector<double>, std__vector_double)
-EXPORT_PROPERTY_NAMED_TYPE(std::vector<std::string>, std__vector_std__string)
-*/
-
 #endif /* PROPERTY_BAG_BOOST_SERIALIZATION_PROPERTY_H */
