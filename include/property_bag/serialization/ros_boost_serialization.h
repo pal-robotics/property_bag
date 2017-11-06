27d19bec6746a791627518060efc73c3f2d381b1
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-11-06 13:37:01 +0100

    ros seria use make_nvp & make_array

--- include/property_bag/serialization/ros_boost_serialization.h
+++ include/property_bag/serialization/ros_boost_serialization.h
@@ -2,7 +2,7 @@
 **
 **  File: ros_boost_serialization
 **
-**  Author: victor
+**  Authors: victor, Jeremie
 **  Created on: 2017/10/24
 **
 **  Copyright (c) 2017 PAL Robotics SL. All Rights Reserved
@@ -12,6 +12,7 @@
 #define ROS_BOOST_SERIALIZATION
 #include <ros/serialization.h>
 #include <boost/serialization/split_free.hpp>
+#include <boost/serialization/array.hpp>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Point.h>
@@ -26,49 +27,46 @@ namespace serialization{
 
 template <class Archive>
 void serialize(Archive & ar, ros::Time &t, const unsigned int version) {
-  ar & t.sec;
-  ar & t.nsec;
+  ar & BOOST_SERIALIZATION_NVP(t.sec);
+  ar & BOOST_SERIALIZATION_NVP(t.nsec);
 }
 
 template <class Archive>
 void serialize(Archive & ar, ros::Duration &d, const unsigned int version) {
-  ar & d.sec;
-  ar & d.nsec;
+  ar & BOOST_SERIALIZATION_NVP(d.sec);
+  ar & BOOST_SERIALIZATION_NVP(d.nsec);
 }
-
 
 #define ROS_BOOST_SERIALIZE_MSG(Msg) \
 template<class Archive>\
 void save(Archive & ar, const Msg &m, const unsigned int version)\
 {\
   ros::SerializedMessage sm = ros::serialization::serializeMessage(m);\
-  ar & sm.num_bytes;\
+  ar & boost::serialization::make_nvp("num_bytes", sm.num_bytes);\
 \
   ptrdiff_t start_offset = sm.message_start - sm.buf.get();\
-  ar & start_offset;\
+  ar & BOOST_SERIALIZATION_NVP(start_offset);\
 \
-  for (size_t i = 0; i < sm.num_bytes; i++)\
-  {\
-    ar & sm.buf[i];\
-  }\
+  ar & boost::serialization::make_nvp("buffer", \
+        boost::serialization::make_array(&sm.buf[0], sm.num_bytes) );\
+\
 }\
 \
 template<class Archive>\
 void load(Archive & ar, Msg &m, const unsigned int version)\
 {\
   size_t num_bytes;\
-  ar & num_bytes;\
+  ar & BOOST_SERIALIZATION_NVP(num_bytes);\
 \
   ptrdiff_t start_offset;\
-  ar & start_offset;\
+  ar & BOOST_SERIALIZATION_NVP(start_offset);\
 \
   ros::SerializedMessage sm(boost::shared_array<uint8_t>(new uint8_t[num_bytes]), num_bytes);\
   sm.message_start = sm.buf.get() + start_offset;\
 \
-  for (size_t i = 0; i < sm.num_bytes; i++)\
-  {\
-    ar & sm.buf[i];\
-  }\
+  ar & boost::serialization::make_nvp("buffer", \
+        boost::serialization::make_array(&sm.buf[0], sm.num_bytes) );\
+\
   ros::serialization::deserializeMessage(sm, m);\
 }\
 \
@@ -76,7 +74,6 @@ template <class Archive>\
 void serialize(Archive & ar, Msg &m, const unsigned int version) {\
   split_free(ar,m,version);\
 }
-
 
 ROS_BOOST_SERIALIZE_MSG(geometry_msgs::Pose)
 ROS_BOOST_SERIALIZE_MSG(geometry_msgs::PoseStamped)
