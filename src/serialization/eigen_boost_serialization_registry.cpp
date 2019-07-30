3d1a445dcbb145d48d3e7a3020a1fb56c47a73a9
Hilario Tome <hilario.tome@pal-robotics.com>
2019-07-30 22:06:49 +0200

    added more propperty bag serialization types

--- src/serialization/eigen_boost_serialization_registry.cpp
+++ src/serialization/eigen_boost_serialization_registry.cpp
@@ -7,3 +7,9 @@ EXPORT_PROPERTY_NAMED_TYPE(Eigen::VectorXd, eigen_vectorxd)
 EXPORT_PROPERTY_NAMED_TYPE(Eigen::Quaterniond, eigen_quaterniond)
 EXPORT_PROPERTY_NAMED_TYPE(Eigen::Isometry3d, eigen_isometry_3d)
 
+EXPORT_PROPERTY_NAMED_TYPE(std::vector<Eigen::Vector3d>, std_vector_eigen_vector3)
+EXPORT_PROPERTY_NAMED_TYPE(std::vector<Eigen::VectorXd>, std_vector_eigen_vectorxd)
+EXPORT_PROPERTY_NAMED_TYPE(std::vector<Eigen::Quaterniond>, std_vector_eigen_quaternion)
+EXPORT_PROPERTY_NAMED_TYPE(std::vector<Eigen::Isometry3d>, std_vector_eigen_isometry)
+
+
