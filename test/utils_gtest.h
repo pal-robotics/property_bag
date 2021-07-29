8e95e18dc4038139c0daa498c4b0fa92f0b595af
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-07-29 12:42:06 +0200

    Replacing PRINTF() that is incomptible with more recent versions of gtest

--- test/utils_gtest.h
+++ test/utils_gtest.h
@@ -27,21 +27,23 @@ namespace testing
 {
 namespace internal
 {
-enum GTestColor
-{
-  COLOR_DEFAULT,
-  COLOR_RED,
-  COLOR_GREEN,
-  COLOR_YELLOW
-};
+// enum GTestColor
+// {
+//   COLOR_DEFAULT,
+//   COLOR_RED,
+//   COLOR_GREEN,
+//   COLOR_YELLOW
+// };
 
-extern void ColoredPrintf(GTestColor color, const char* fmt, ...);
-
-#define PRINTF(...) \
-  do { testing::internal::ColoredPrintf(testing::internal::COLOR_GREEN,\
-  "[          ] "); \
-  testing::internal::ColoredPrintf(testing::internal::COLOR_YELLOW, __VA_ARGS__); } \
-  while(0)
+// extern void ColoredPrintf(GTestColor color, const char* fmt, ...);
+/*
+// #define PRINTF(...) \
+//   do { testing::internal::ColoredPrintf(testing::internal::COLOR_GREEN,\
+//   "[          ] "); \
+//   testing::internal::ColoredPrintf(testing::internal::COLOR_YELLOW, __VA_ARGS__); } \
+//   while(0)
+*/
+#define PRINTF(...) printf(__VA_ARGS__)
 
 // C++ stream interface
 class TestCout : public std::stringstream
